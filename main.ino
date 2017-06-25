106fcf73e917000f628a47b11379f5b8b72a4a3a
dip56245 <dip56245@gmail.com>
2017-06-25 22:06:36 +0300

    add WiFi manager

--- main.ino
+++ main.ino
@@ -1,34 +1,39 @@
-// mqtt_esp8266_ds1820_arduino
-// A simple Sketch to read the Temperature from multiple DS18B20 and publish them to a MQTT-Server using a ESP8266.
-// Compiles in the Arduino IDE for the ESP8266
-// Based on:
-// - https://github.com/esp8266/Arduino
-// - https://github.com/knolleary/pubsubclient
-// - https://gist.github.com/igrr/7f7e7973366fc01d6393
-// - The example Sketch for the DS18B20 from the Arduino IDE
+/**
+ * dip56245 - IoT termometer
+ * 
+ * used: https://github.com/tzapu/WiFiManager
+ * https://github.com/knolleary/pubsubclient
+ */
 
+#include <FS.h>                   //this needs to be first, or it all crashes and burns...
+#include <ESP8266WiFi.h>          //ESP8266 Core WiFi Library (you most likely already have this in your sketch)
+#include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
+#include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
+#include <ArduinoJson.h>          //https://github.com/bblanchon/ArduinoJson
 #include <PubSubClient.h>
-#include <ESP8266WiFi.h>
 #include <OneWire.h>
 
-//#define ONE_WIRE_BUS D1
-#define ONE_WIRE_BUS 4
-#define sleepTimeS 60*30
 
-//Basic Wifi and MQTT-Server Information
-const char* ssid = "MyNetWork";
-const char* password = "***";
-const char* mqtt_server = "192.168.0.1";
+#define WELCOME_TEXT  "\n-=[ 20170421_IoT_ds18b20 (c) dip56245 ]=\n                  __________________________________________________\n           ____.-\"\":\":\":\":\":\":\":\":\":\":\":\":\":\":\":\":\":\":\":\":\":\":\":\":\":\"-.\n          (___:===='==='==='==='==='==='==='=A '   '   '   '   '   '   )\n          jgs `'-._92____94____96____8__|_100____2_____4_____6_____8.-`"
+#define BUTTON_PIN 12
+#define ONE_WIRE_BUS 13
+
+char mqtt_server[40] = "192.168.0.1";
+char mqtt_port[6] = "1883";
+char mqtt_interval[5] = "600";
+bool shouldSaveConfig = false;
 
 OneWire  ds(ONE_WIRE_BUS);
 WiFiClient wifiClient;
 PubSubClient client(wifiClient);
 
-void callback(char* topic, byte* payload, unsigned int length) {
-  // handle message arrived
-  Serial.println("data...");
+void saveConfigCallback () {
+  Serial.println("Should save config");
+  shouldSaveConfig = true;
 }
 
+void callback(char* topic, byte* payload, unsigned int length) {}
 
 String macToStr(const uint8_t* mac){
   String result;
@@ -41,25 +46,94 @@ String macToStr(const uint8_t* mac){
 }
 
 void setup() {
-  Serial.begin(9600);
-  delay(10);
-  
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  
-  WiFi.begin(ssid, password);
-  
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  Serial.begin(115200);
+  Serial.println(WELCOME_TEXT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+  WiFiManager wifiManager;
+  if (digitalRead(BUTTON_PIN)==false){
+    Serial.print("RESETING Data....");
+    wifiManager.resetSettings();
+    SPIFFS.format();
+    Serial.println("ok");
+    delay(1000);
   }
-  Serial.println("");
-  Serial.println("WiFi connected");  
-  Serial.println("IP address: ");
+  wifiManager.setBreakAfterConfig(true);
+
+  if (SPIFFS.begin()){
+    Serial.println("mounted file system");
+    if (SPIFFS.exists("/config.json")) {
+      //file exists, reading and loading
+      Serial.println("reading config file");
+      File configFile = SPIFFS.open("/config.json", "r");
+      if (configFile) {
+        Serial.println("opened config file");
+        size_t size = configFile.size();
+        // Allocate a buffer to store contents of the file.
+        std::unique_ptr<char[]> buf(new char[size]);
+
+        configFile.readBytes(buf.get(), size);
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject& json = jsonBuffer.parseObject(buf.get());
+        json.printTo(Serial);
+        if (json.success()) {
+          Serial.println("\nparsed json");
+          strcpy(mqtt_server, json["mqtt_server"]);
+          strcpy(mqtt_port, json["mqtt_port"]);
+          strcpy(mqtt_interval, json["mqtt_interval"]);
+        } else {
+          Serial.println("failed to load json config");
+        }
+      }
+    }
+  } else {
+    Serial.println("failed to mount FS");
+  }
+
+  WiFiManagerParameter custom_mqtt_server("server", "mqtt server", mqtt_server, 40);
+  WiFiManagerParameter custom_mqtt_port("port", "mqtt port", mqtt_port, 5);
+  WiFiManagerParameter custom_mqtt_interval("interval", "send data interval (seconds)", mqtt_interval, 5);
+  wifiManager.setSaveConfigCallback(saveConfigCallback);
+
+  //add all your parameters here
+  wifiManager.addParameter(&custom_mqtt_server);
+  wifiManager.addParameter(&custom_mqtt_port);
+  wifiManager.addParameter(&custom_mqtt_interval);
+  
+  if (!wifiManager.autoConnect("IOTTEMP", "password")) {
+    Serial.println("failed to connect, we should reset as see if it connects");
+    delay(3000);
+    ESP.reset();
+    delay(5000);
+  }
+
+  //read updated parameters
+  strcpy(mqtt_server, custom_mqtt_server.getValue());
+  strcpy(mqtt_port, custom_mqtt_port.getValue());
+  strcpy(mqtt_interval, custom_mqtt_interval.getValue());
+  if (shouldSaveConfig) {
+    Serial.println("saving config");
+    DynamicJsonBuffer jsonBuffer;
+    JsonObject& json = jsonBuffer.createObject();
+    json["mqtt_server"] = mqtt_server;
+    json["mqtt_port"] = mqtt_port;
+    json["mqtt_interval"] = mqtt_interval;
+
+    File configFile = SPIFFS.open("/config.json", "w");
+    if (!configFile) {
+      Serial.println("failed to open config file for writing");
+    }
+
+    json.printTo(Serial);
+    json.printTo(configFile);
+    configFile.close();
+    //end save
+  }
+  Serial.println("connected...yeey :)");
+  Serial.println("local ip");
   Serial.println(WiFi.localIP());
 
+  //connect to mqtt
   // Generate client name based on MAC address and last 8 bits of microsecond counter
   String clientName;
   clientName += "esp8266-";
@@ -69,8 +143,7 @@ void setup() {
   clientName += "-";
   clientName += String(micros() & 0xff, 16);
 
-  client.setServer(mqtt_server, 1883);
-
+  client.setServer(mqtt_server, atoi(mqtt_port));
   Serial.print("Connecting to ");
   Serial.print(mqtt_server);
   Serial.print(" as ");
@@ -86,112 +159,105 @@ void setup() {
   }
 }
 
-void loop() {
+void findAndPost(){
   byte i;
   byte present = 0;
   byte type_s;
   byte data[12];
   byte addr[8];
-  
 
-//Loop through all DS1820
+  while(ds.search(addr)){ 
+    Serial.print("ROM =");
+    String romcode = "/temp/";
+    for( i = 0; i < 8; i++) {
+      Serial.write(' ');
+      Serial.print(addr[i], HEX);
+      romcode = romcode + String(addr[i], HEX);
+    }
 
-while(ds.search(addr)){ 
-  Serial.print("ROM =");
-
-//Topic is built from a static String plus the ID of the DS18B20
-  String romcode = "/temp/";
-  for( i = 0; i < 8; i++) {
-    Serial.write(' ');
-    Serial.print(addr[i], HEX);
-    romcode = romcode + String(addr[i], HEX);
-  }
-
-  if (OneWire::crc8(addr, 7) != addr[7]) {
-      Serial.println("CRC is not valid!");
-      return;
-  }
-  Serial.println();
+    if (OneWire::crc8(addr, 7) != addr[7]) {
+        Serial.println("CRC is not valid!");
+        return;
+    }
+    Serial.println();
  
-// the first ROM byte indicates which chip
-  switch (addr[0]) {
-    case 0x10:
-      Serial.println("  Chip = DS18S20");  // or old DS1820
-      type_s = 1;
-      break;
-    case 0x28:
-      Serial.println("  Chip = DS18B20");
-      type_s = 0;
-      break;
-    case 0x22:
-      Serial.println("  Chip = DS1822");
-      type_s = 0;
-      break;
-    default:
-      Serial.println("Device is not a DS18x20 family device.");
-      return;
-  } 
+    switch (addr[0]) {
+      case 0x10:
+        Serial.println("  Chip = DS18S20");  // or old DS1820
+        type_s = 1;
+        break;
+      case 0x28:
+        Serial.println("  Chip = DS18B20");
+        type_s = 0;
+        break;
+      case 0x22:
+        Serial.println("  Chip = DS1822");
+        type_s = 0;
+        break;
+      default:
+        Serial.println("Device is not a DS18x20 family device.");
+        return;
+    } 
 
-  ds.reset();
-  ds.select(addr);
-  ds.write(0x44, 1);        // start conversion, with parasite power on at the end
-  
-  delay(1000);     // maybe 750ms is enough, maybe not
-  // we might do a ds.depower() here, but the reset will take care of it.
-  
-  present = ds.reset();
-  ds.select(addr);    
-  ds.write(0xBE);         // Read Scratchpad
+    ds.reset();
+    ds.select(addr);
+    ds.write(0x44, 1);        // start conversion, with parasite power on at the end
+    
+    delay(1000);     // maybe 750ms is enough, maybe not
+    // we might do a ds.depower() here, but the reset will take care of it.
+    
+    present = ds.reset();
+    ds.select(addr);    
+    ds.write(0xBE);         // Read Scratchpad
 
-  Serial.print("  Data = ");
-  Serial.print(present, HEX);
-  Serial.print(" ");
-  for ( i = 0; i < 9; i++) {           // we need 9 bytes
-    data[i] = ds.read();
-    Serial.print(data[i], HEX);
+    Serial.print("  Data = ");
+    Serial.print(present, HEX);
     Serial.print(" ");
-  }
-  Serial.print(" CRC=");
-  Serial.print(OneWire::crc8(data, 8), HEX);
-  Serial.println();
-
-  int16_t raw = (data[1] << 8) | data[0];
-  if (type_s) {
-    raw = raw << 3; // 9 bit resolution default
-    if (data[7] == 0x10) {
-      // "count remain" gives full 12 bit resolution
-      raw = (raw & 0xFFF0) + 12 - data[6];
+    for ( i = 0; i < 9; i++) {           // we need 9 bytes
+      data[i] = ds.read();
+      Serial.print(data[i], HEX);
+      Serial.print(" ");
     }
-  } else {
-    byte cfg = (data[4] & 0x60);
-    // at lower res, the low bits are undefined, so let's zero them
-    if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
-    else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
-    else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
-    //// default is 12 bit resolution, 750 ms conversion time
-  }
+    Serial.print(" CRC=");
+    Serial.print(OneWire::crc8(data, 8), HEX);
+    Serial.println();
 
-  //convert RAW Temperature to String
-  //String raw_temp = String(raw, DEC);
-  int celsius = (float)raw/16.0;
-  String raw_temp = String(celsius, DEC);
+    int16_t raw = (data[1] << 8) | data[0];
+    if (type_s) {
+      raw = raw << 3; // 9 bit resolution default
+      if (data[7] == 0x10) {
+        // "count remain" gives full 12 bit resolution
+        raw = (raw & 0xFFF0) + 12 - data[6];
+      }
+    } else {
+      byte cfg = (data[4] & 0x60);
+      // at lower res, the low bits are undefined, so let's zero them
+      if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
+      else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
+      else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
+      //// default is 12 bit resolution, 750 ms conversion time
+    }
 
-  if (client.connected()){
-    Serial.print("Sending payload: ");
-    Serial.println(raw_temp);
+    //convert RAW Temperature to String
+    //String raw_temp = String(raw, DEC);
+    int celsius = (float)raw/16.0;
+    String raw_temp = String(celsius, DEC);
+  
+    if (client.connected()){
+      Serial.print("Sending payload: ");
+      Serial.println(raw_temp);
+    }
+      if (client.publish((char*) romcode.c_str(), (char*) raw_temp.c_str())) {
+        Serial.println("Publish ok");
+      } else {
+        Serial.println("Publish failed");
+      }
+      delay(1000);
   }
-    if (client.publish((char*) romcode.c_str(), (char*) raw_temp.c_str())) {
-      Serial.println("Publish ok");
-    }
-    else {
-      Serial.println("Publish failed");
-    }
-    delay(1000);
 }
-  //End of the OneWire-Devices, reset Loop
-  Serial.println("End of Onewire Bus");
-  ds.reset_search();
-  ESP.deepSleep(sleepTimeS * 1000000);
+
+void loop() {
+  findAndPost();
+  ESP.deepSleep(atoi(mqtt_interval)*1000000);
+  Serial.println("error sleep");
 }
-
-
